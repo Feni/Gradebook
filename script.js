f53a8dd6bb3c6c34a67e2a934ab0ab3983af570b
Feni Varughese <feni727@gmail.com>
2012-11-07 23:29:09 -0600

    Imported new logo in

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function addGradeField() {
-	var gradesList = document.getElementById("HomeworkGrades");
+function addGradeField(category) {
+	var gradesList = document.getElementById(category+"Grades");
 	var newGrade = document.createElement("li");
 	var idCode = gradesList.childNodes.length - 2;
 	
 	/// A little quick fix to fix IDs. It messes up because the active marker throws off the count for everything after the first one...
 	if(idCode > 1)
-		idCode = idCode-1;
+		idCode = (idCode+1)/2;
 	
-	newGrade.setAttribute("id","Grade"+idCode);
+	newGrade.setAttribute("id",category+"Grade"+idCode);
 	newGrade.setAttribute("class", "Grade");
-	newGrade.innerHTML = '<input type="text"class="GradeName" value="Homework #"/><input type="text" class="GradeValue" id="GradeValue'+idCode+'" value = "100" />';
+	newGrade.innerHTML = '<input type="text"class="GradeName" value="'+category+' #"/><input type="text" class="GradeValue" id="'+category+'GradeValue'+idCode+'" value = "100" />';
 	
 	var activeMarker = document.createElement('input');    
 	activeMarker.type = "checkbox";
-	activeMarker.id = "GradeMarker"+idCode;
+	activeMarker.id = ""+category+"GradeMarker"+idCode;
 	activeMarker.setAttribute("class", "ActiveMarker");
 	activeMarker.setAttribute("Checked", "True");	
 	gradesList.appendChild(activeMarker);
@@ -22,10 +22,25 @@ function addGradeField() {
 }
 
 function init(){
-	setInterval('calculateGrade()', 1000);
+	setInterval('calculateTotal()', 3000);
 }
 
-function calculateGrade()
+function calculateTotal()
+{
+	// This could be converted into a for loop, sure... but too much code overhead... This is cleaner and more efficient. 
+	var homeworkGrade = calculateGrade("Homework") * parseFloat(document.getElementById("HomeworkPercentage").value)/100.0;
+	var quizGrade = calculateGrade("Quiz") * parseFloat(document.getElementById("QuizPercentage").value)/100.0;
+	var testGrade = calculateGrade("Test") * parseFloat(document.getElementById("TestPercentage").value)/100.0;
+	var projectGrade = calculateGrade("Project") * parseFloat(document.getElementById("ProjectPercentage").value)/100.0;
+	var otherGrade = calculateGrade("Other") * parseFloat(document.getElementById("OtherPercentage").value)/100.0;
+	var finalGrade = calculateGrade("Final") * parseFloat(document.getElementById("FinalPercentage").value)/100.0;
+	
+	var total = ( homeworkGrade + quizGrade + testGrade + projectGrade ) * 100.0;
+	
+	document.getElementById("TotalTotalSummary").innerHTML = total;
+}
+
+function calculateGrade(category)
 {
 	// Todo: add grade/total field, instead of assuming it's out of 100...
 //	var gradeValue = document.getElementById('myText');
@@ -33,7 +48,7 @@ function calculateGrade()
 
 	var i = 1;
 
-        var gradeValue = document.getElementById("GradeValue"+i);
+        var gradeValue = document.getElementById(category+"GradeValue"+i);
         
         var grade = 0;
         var total = 0;
@@ -41,15 +56,16 @@ function calculateGrade()
        	while( gradeValue != null){
        		// Do stuff with the grade value. 
        		
-       		var activeCheck = document.getElementById("GradeMarker"+i);
+       		var activeCheck = document.getElementById(category+"GradeMarker"+i);
        		if(activeCheck.checked){		// Check if we shoudl include that grade in...
 	       		grade+=parseFloat(gradeValue.value);	// Note: Do parseInt here instead?
 	       		total+=100;
        		}
        		i++;
-       		gradeValue = document.getElementById("GradeValue"+i);
+       		gradeValue = document.getElementById(category+"GradeValue"+i);
 	}
-	
-	document.getElementById("HomeworkTotal").innerHTML = ""+grade+"/"+total;
+	document.getElementById(category+"Total").innerHTML = ""+grade+"/"+total;
+	document.getElementById(category+"TotalSummary").innerHTML = ""+grade+"/"+total;
+	return grade/total;
 }
 
